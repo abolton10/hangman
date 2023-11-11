4ac82bd6a21173b62c0918465f8f7cd56eec926a
abolton10 <147441705+abolton10@users.noreply.github.com>
2023-11-11 12:53:50 -0500

    Update hangman-test.php

--- hangman-test.php
+++ hangman-test.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-//If the game is lost, takes back to index
+// If the game is lost, takes back to index
 if (isset($_GET['reset']) && $_GET['reset'] === 'true') {
     unset($_SESSION['word']);
     unset($_SESSION['guessed']);
@@ -25,7 +25,6 @@ if (isset($_GET['next']) && $_GET['next'] === 'true') {
 // Define an array with word lists for different levels
 $wordLists = [
     ['mania', 'disco', 'plead', 'glove', 'exert', 'chair', 'happy'],
-    
 ];
 
 // Check if the current level exists in the word lists
@@ -80,43 +79,50 @@ function displayWord() {
 </head>
 <body>
     <div class="container">
-    <div class="header">
-        <h1>Hangman</h1>
-    </div>
+        <div class="header">
+            <h1><u>H_ngm_n</u></h1>
+        </div>
 
-    <?php
-    if ($_SESSION['isGameOver']) {
-        echo "<h2>Game Over</h2>";
-        echo "<p>The word was: " . implode('', $_SESSION['word']) . "</p>";
-        echo '<a href="?reset=true"><button type="button">Try Again</button></a>';
-        // Check if there is another level to move to
-        if (isset($wordLists[$currentLevel + 1])) {
-            echo '<a href="?next=true"><button type="button">Next Level</button></a>';
-        } else {
-            echo "You've completed all levels!";
-        }
-    } else {
-        echo "<h2>Word: " . displayWord() . "</h2>";
-        echo "<p>Attempts left: " . $_SESSION['attempts'] . "</p>";
-    }
-    ?>
-    
-    <form method="post" action="">
-        <label for="guess">Guess a letter:</label>
-        <input type="text" name="guess" maxlength="1" pattern="[A-Za-z]" required>
-        <input type="submit" value="Submit">
-        <?php if ($_SESSION['isGameOver']): ?>
-            <a href="?reset=true"><button type="button">Try Again</button></a>
-        <?php endif; ?>
-    </form>
-    <div class="keyboard">
         <?php
-        $alphabet = range('A', 'Z');
-        foreach ($alphabet as $letter) {
-            $cssClass = in_array($letter, $_SESSION['guessed']) ? 'guessed' : '';
-            echo "<div class='key $cssClass'>$letter</div>";
+        if ($_SESSION['isGameOver']) {
+            echo "<h2>Game Over</h2>";
+            echo "<p>The word was: " . implode('', $_SESSION['word']) . "</p>";
+            echo '<a href="?reset=true"><button type="button">Try Again</button></a>';
+            
+            // Check if there is another level to move to
+            if (isset($wordLists[$currentLevel + 1])) {
+                echo '<a href="?next=true"><button type="button">Next Level</button></a>';
+            } else {
+                echo "You've completed all levels!";
+            }
+        } else {
+            echo "<h2>Word: " . displayWord() . "</h2>";
+            echo "<p>Attempts left: " . $_SESSION['attempts'] . "</p>";
         }
         ?>
+        
+        <form method="post" action="">
+            <label for="guess">Guess a letter:</label>
+            <input type="text" name="guess" maxlength="1" pattern="[A-Za-z]" required>
+            <input type="submit" value="Submit">
+            
+            <?php if ($_SESSION['isGameOver']): ?>
+                <a href="?reset=true"><button type="button">Try Again</button></a>
+            <?php endif; ?>
+        </form>
+        
+        <div class="guessed-container">
+            <div class="letters-guessed">
+                <p>Letters Used:</p>
+                <div class="letters">
+                    <?php
+                    foreach ($_SESSION['guessed'] as $guessedLetter) {
+                        echo "<span>$guessedLetter</span>";
+                    }
+                    ?>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
