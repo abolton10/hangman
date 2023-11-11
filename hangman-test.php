2735ac622b43a324964f9fb4dce9b60b2efff4b9
abolton10 <147441705+abolton10@users.noreply.github.com>
2023-11-11 14:15:20 -0500

    Update hangman-test.php

--- hangman-test.php
+++ hangman-test.php
@@ -1,3 +1,97 @@
+<?php
+session_start();
+
+// If the game is lost, take back to index
+if (isset($_GET['reset']) && $_GET['reset'] === 'true') {
+    unset($_SESSION['word']);
+    unset($_SESSION['guessed']);
+    unset($_SESSION['attempts']);
+    unset($_SESSION['isGameOver']);
+    header('Location: index-hm.php');
+    exit;
+}
+
+// Check if the current level is completed
+if (isset($_GET['next']) && $_GET['next'] === 'true') {
+    $_SESSION['currentLevel'] = ($_SESSION['currentLevel'] ?? 1) + 1;
+    unset($_SESSION['word']);
+    unset($_SESSION['guessed']);
+    unset($_SESSION['attempts']);
+    unset($_SESSION['isGameOver']);
+    header('Location: index-hm.php');
+    exit;
+}
+
+// Get the word list based on the selected game mode
+function getWordList($gameMode) {
+    $filename = "{$gameMode}.txt";
+    if (!file_exists($filename)) {
+        echo "Word file not found!";
+        exit;
+    }
+
+    return file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+}
+
+// Set the game mode (default to easy if not specified)
+$gameMode = $_GET['mode'] ?? 'easy';
+
+// Check if the game mode exists
+if (!in_array($gameMode, ['easy', 'medium', 'hard'])) {
+    echo "Invalid game mode!";
+    exit;
+}
+
+// Check if the current level exists in the word lists
+$currentLevel = $_SESSION['currentLevel'] ?? 0;
+$wordLists = [
+    'easy'   => ['word1', 'word2', 'word3'], // Replace with your actual word list for easy mode
+    'medium' => ['word4', 'word5', 'word6'], // Replace with your actual word list for medium mode
+    'hard'   => ['word7', 'word8', 'word9'], // Replace with your actual word list for hard mode
+];
+
+if (!isset($wordLists[$gameMode][$currentLevel])) {
+    echo "You've completed all levels!";
+    exit;
+}
+
+if (!isset($_SESSION['word'])) {
+    $wordList = $wordLists[$gameMode];
+    $_SESSION['word'] = str_split(strtoupper($wordList[$currentLevel]));
+    $_SESSION['guessed'] = [];
+    $_SESSION['attempts'] = 6;
+    $_SESSION['isGameOver'] = false;
+}
+
+if ($_SESSION['attempts'] === 0 || count(array_intersect($_SESSION['word'], $_SESSION['guessed'])) === count(array_unique($_SESSION['word']))) {
+    $_SESSION['isGameOver'] = true;
+}
+
+if (isset($_POST['guess']) && !$_SESSION['isGameOver']) {
+    $letter = strtoupper($_POST['guess']);
+
+    if (!in_array($letter, $_SESSION['guessed'])) {
+        array_push($_SESSION['guessed'], $letter);
+
+        if (!in_array($letter, $_SESSION['word'])) {
+            $_SESSION['attempts']--;
+        }
+    }
+}
+
+function displayWord() {
+    $display = '';
+    foreach ($_SESSION['word'] as $letter) {
+        if (in_array($letter, $_SESSION['guessed'])) {
+            $display .= $letter;
+        } else {
+            $display .= '_';
+        }
+    }
+    return $display;
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -51,6 +145,10 @@
                 </div>
             </div>
         </div>
+    </div>
+</body>
+</html>
+
     </div>
 </body>
 </html>
