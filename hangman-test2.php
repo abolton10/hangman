8ee1215aca3797fe0e6d1908095f30433dc7dce7
Christy Mugomba <108655633+christymugs@users.noreply.github.com>
2023-11-13 13:46:12 -0500

    Update hangman-test2.php
    updated with next level

--- hangman-test2.php
+++ hangman-test2.php
@@ -1,12 +1,35 @@
 <?php
 session_start();
 
-// Set the game mode (default to easy if not specified)
-$gameMode = $_GET['mode'] ?? 'easy';
+$wordGuessed = false;
 
-// Check if the game mode exists
-if (!in_array($gameMode, ['easy', 'medium', 'hard'])) {
-    echo "Invalid game mode!";
+// If the game is lost or reset, take back to index
+if (isset($_GET['reset']) && $_GET['reset'] === 'true') {
+    unset($_SESSION['word']);
+    unset($_SESSION['guessed']);
+    unset($_SESSION['attempts']);
+    unset($_SESSION['isGameOver']);
+    $_SESSION['currentLevel'] = 0; // Set the level to 0 when resetting
+    header('Location: hangman-test.php');
+    exit;
+}
+
+// Check if the current level is completed
+if (isset($_GET['next']) && $_GET['next'] === 'true') {
+    // Check if the word is guessed correctly before moving to the next level
+    if (count(array_intersect($_SESSION['word'], $_SESSION['guessed'])) === count(array_unique($_SESSION['word']))) {
+        $_SESSION['currentLevel'] = ($_SESSION['currentLevel'] ?? 0) + 1; // Increment the level
+        $wordGuessed = true;
+    } else {
+        // Reset the current level if the word is guessed incorrectly
+        $_SESSION['currentLevel'] = 0;
+    }
+
+    unset($_SESSION['word']);
+    unset($_SESSION['guessed']);
+    unset($_SESSION['attempts']);
+    unset($_SESSION['isGameOver']);
+    header('Location: hangman-test.php');
     exit;
 }
 
@@ -21,34 +44,21 @@ function getWordList($gameMode) {
     return file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 }
 
-// If the game is lost or reset, take back to index
-if (isset($_GET['reset']) && $_GET['reset'] === 'true') {
-    unset($_SESSION['word']);
-    unset($_SESSION['guessed']);
-    unset($_SESSION['attempts']);
-    unset($_SESSION['isGameOver']);
-    $_SESSION['currentLevel'] = 0; // Set the level to 0 when resetting
-    header('Location: index-hm.php');
-    exit;
-}
+// Set the game mode (default to easy if not specified)
+$gameMode = $_GET['mode'] ?? 'easy';
 
-// Check if the current level is completed
-if (isset($_GET['next']) && $_GET['next'] === 'true') {
-    $_SESSION['currentLevel'] = ($_SESSION['currentLevel'] ?? 0) + 1; // Increment the level
-    unset($_SESSION['word']);
-    unset($_SESSION['guessed']);
-    unset($_SESSION['attempts']);
-    unset($_SESSION['isGameOver']);
-    header('Location: index-hm.php');
+// Check if the game mode exists
+if (!in_array($gameMode, ['easy', 'medium', 'hard'])) {
+    echo "Invalid game mode!";
     exit;
 }
 
 // Check if the current level exists in the word lists
 $currentLevel = $_SESSION['currentLevel'] ?? 0;
 $wordLists = [
-    'easy'   => ['easy','bags','money'], // Replace with your actual word list for easy mode
-    'medium' => ['medium','headphone','ipad'], // Replace with your actual word list for medium mode
-    'hard'   => ['hard','cousin','soap'], // Replace with your actual word list for hard mode
+    'easy'   => ['easy', 'bags', 'money'], // Replace with your actual word list for easy mode
+    'medium' => ['medium', 'headphone', 'ipad'], // Replace with your actual word list for medium mode
+    'hard'   => ['hard', 'cousin', 'soap'], // Replace with your actual word list for hard mode
 ];
 
 if (!isset($wordLists[$gameMode][$currentLevel])) {
@@ -79,9 +89,10 @@ if (isset($_POST['guess']) && !$_SESSION['isGameOver']) {
             $_SESSION['attempts']--;
         }
     }
-    
+
     if (count(array_intersect($_SESSION['word'], $_SESSION['guessed'])) === count(array_unique($_SESSION['word']))) {
         $_SESSION['isGameOver'] = true;
+        $wordGuessed = true;
     }
 }
 
@@ -99,36 +110,36 @@ function displayWord() {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hangman</title>
     <link rel="stylesheet" type="text/css" href="project2.css">
 </head>
 <body>
-    <div class="container">
+    <div class="container <?php echo ($wordGuessed) ? 'confetti' : ''; ?>">
         <div class="header">
             <h1><u>H_ngm_n</u></h1>
             <h2>Level <?php echo $currentLevel + 1; ?></h2>
         </div>
 
-        <?php
-        if ($_SESSION['isGameOver']) {
-            echo "<h2>Game Over</h2>";
-            echo "<p>The word was: " . implode('', $_SESSION['word']) . "</p>";
+        <?php if ($_SESSION['isGameOver']): ?>
+            <h2>Game Over</h2>
+            <p>The word was: <?php echo implode('', $_SESSION['word']); ?></p>
+            <!-- Single "Try Again" button that resets the current level -->
+            <a href="?reset=true"><button type="button">Try Again</button></a>
 
-            // Check if there is another level to move to
-            if (isset($wordLists[$gameMode][$currentLevel + 1])) {
-                echo '<a href="?reset=true"><button type="button">Try Again</button></a>';
-                echo '<a href="?next=true"><button type="button">Next Level</button></a>';
-            } else {
-                echo '<a href="?reset=true"><button type="button">Try Again</button></a>';
-                echo "You've completed all levels!";
-            }
-        } else {
-            echo "<h2>Word: " . displayWord() . "</h2>";
-            echo "<p>Attempts left: " . $_SESSION['attempts'] . "</p>";
-        }
-        ?>
+            <!-- Check if there is another level to move to -->
+            <?php if (isset($wordLists[$gameMode][$currentLevel + 1])): ?>
+                <a href="?next=true"><button type="button">Next Level</button></a>
+            <?php else: ?>
+                You've completed all levels!
+            <?php endif; ?>
+        <?php else: ?>
+            <h2>Word: <?php echo displayWord(); ?></h2>
+            <p>Attempts left: <?php echo $_SESSION['attempts']; ?></p>
+        <?php endif; ?>
 
         <form method="post" action="">
             <?php if (!$_SESSION['isGameOver']): ?>
