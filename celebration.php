2a11f9e332d266cf01482f4a8594b41708778123
AndresZ1129 <145486662+AndresZ1129@users.noreply.github.com>
2023-11-13 21:22:18 -0500

    Add files via upload

---
+++ celebration.php
@@ -0,0 +1,23 @@
+<?php
+?>
+<html>
+    <link rel="stylesheet" type="text/css" href="project2.css">
+    <head>
+        <title>Celebration</title>
+    </head>
+    <body>
+        <div class="container">
+        <h1>Congratulations! Thank you for playing H_ngm_n!</h2>
+        <div class="wrapper">
+            <div class="head"></div>
+            <div class="torso"></div>
+            <div class="leftarm"></div>
+            <div class="rightarm"></div>
+            <div class="leftleg"></div>
+            <div class="leftfoot"></div>
+            <div class="rightleg"></div>
+            <div class="rightfoot"></div>
+        </div> 
+        </div>
+    </body>
+</html>
