901ab8cc7f567f70ab63b783863bf8c4e5282b98
Christy Mugomba <108655633+christymugs@users.noreply.github.com>
2023-11-16 19:28:37 -0500

    Update level2.php
    debugging

--- level2.php
+++ level2.php
@@ -1,38 +1,59 @@
 <?php
 session_start();
 
-// Set the level to 2
-$_SESSION['currentLevel'] = 1; // Level 2
+// Set the level to 1 if it's not set
+$_SESSION['currentLevel'] = $_SESSION['currentLevel'] ?? 1;
 
-function getMediumWordList() {
+// Check if the reset parameter is set in the URL and reset the session
+if (isset($_GET['reset']) && $_GET['reset'] === 'true') {
+    unset($_SESSION['word']);
+    unset($_SESSION['guessed']);
+    unset($_SESSION['attempts']);
+    unset($_SESSION['isGameOver']);
+    $_SESSION['currentLevel'] = 1; // Set the level to 1 when resetting
+    $_SESSION['wins'] = 1; // Reset the wins counter
+    header('Location: level2.php');
+    exit;
+}
+
+// Initialize the wins counter if it's not set
+$_SESSION['wins'] = $_SESSION['wins'] ?? 1;
+
+function getRandomWordList($count = 3) {
     $filename = 'medium.txt';
     if (!file_exists($filename)) {
         echo "Word file not found!";
         exit;
     }
+    $wordList = file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-    return file($filename, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    // Shuffle the word list and select the first $count words
+    shuffle($wordList);
+    return array_slice($wordList, 0, $count);
 }
 
 if (!isset($_SESSION['word'])) {
-    $wordList = getMediumWordList();
-    $selectedWord = $wordList[array_rand($wordList)];
+    $wordList = getRandomWordList();
+
+    // Set the word for the current level
+    $selectedWord = $wordList[$_SESSION['currentLevel'] - 1];
+
     $_SESSION['word'] = str_split(strtoupper($selectedWord));
     $_SESSION['guessed'] = [];
     $_SESSION['attempts'] = 6;
     $_SESSION['isGameOver'] = false;
 }
 
-$shakingClass = '';
-
 // Process the guess
+$shakingClass = ''; // Initialize the variable
+
 if (isset($_POST['guess']) && !$_SESSION['isGameOver']) {
     $letter = strtoupper($_POST['guess']);
     if (!in_array($letter, $_SESSION['guessed'])) {
         array_push($_SESSION['guessed'], $letter);
         if (!in_array($letter, $_SESSION['word'])) {
             $_SESSION['attempts']--;
-            $shakingClass = 'shake';
+            $shakingClass = 'shake'; // Add this class for the shaking animation
         }
     }
 }
@@ -49,9 +70,12 @@ foreach ($_SESSION['word'] as $letter) {
 if ($_SESSION['attempts'] === 0 || $wordGuessed) {
     $_SESSION['isGameOver'] = true;
     if ($_SESSION['attempts'] === 0) {
+        // Play the boo.mp3 sound effect
         echo '<audio autoplay><source src="boo.mp3" type="audio/mpeg"></audio>';
     }
 }
+
+// Check for victory after completing level 3
 
 function displayWord() {
     $display = '';
@@ -69,14 +93,14 @@ function displayWord() {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hangman - Level 2</title>
+    <title>Hangman - Level <?php echo $_SESSION['wins']; ?></title>
     <link rel="stylesheet" type="text/css" href="project2.css">
 </head>
 <body>
     <div class="container">
         <div class="header">
             <h1><u>H_ngm_n</u></h1>
-            <h2>Level 2</h2>
+            <h2>Level <?php echo $_SESSION['wins']; ?> (Medium)</h2>
         </div>
         <div class="bg1 <?php echo $shakingClass; ?>">
             <?php
@@ -91,21 +115,46 @@ function displayWord() {
         <?php
         if ($_SESSION['isGameOver']) {
             if ($wordGuessed) {
-                echo "<h2>Congratulations! You guessed the word!</h2>";
-                echo '<a href="?next=true"><button type="button">Go to Level 3</button></a>';
+                echo "<h2>Congratulations! You guessed the word! One more level to go!</h2>";
+                echo "<a href='index-hm.php'><button type='button'>Take me Home</button></a>";
+                $_SESSION['currentLevel']++;
+                $_SESSION['wins']++; // Increment the wins counter
+                if ($_SESSION['currentLevel'] <= 3) {
+                    // Reset the session for the next word
+                    unset($_SESSION['word']);
+                    unset($_SESSION['guessed']);
+                    unset($_SESSION['attempts']);
+                    unset($_SESSION['isGameOver']);
+                    header("Location: level2.php");
+                    exit;
+                }
+                if ($_SESSION['wins'] > 3) {
+                    echo "<a href='level3.php'><button type='button'>Play Level 3</button></a>";
+                    // Reset the session for a new game
+                    unset($_SESSION['word']);
+                    unset($_SESSION['guessed']);
+                    unset($_SESSION['attempts']);
+                    unset($_SESSION['isGameOver']);
+                    $_SESSION['currentLevel'] = 1;
+                    $_SESSION['wins'] = 1;
+                    exit;
+}
             } else {
                 echo "<h2>Game Over</h2>";
                 echo "<p>The word was: " . implode('', $_SESSION['word']) . "</p>";
-                echo '<form method="post" action="level1.php"><input type="submit" value="Try Again"></form>';
+                if ($_SESSION['wins'] == 1) {
+                    echo "<a href='level1.php?reset=true'><button type='button'>Try Again</button></a>";
+                }
             }
         } else {
+            // Display the current level information
             echo "<h2>Word: " . displayWord() . "</h2>";
             echo "<p>Attempts left: " . $_SESSION['attempts'] . "</p>";
-            echo '<form method="post" action="">';
-            echo '<label for="guess">Guess a letter:</label>';
-            echo '<input type="text" name="guess" maxlength="1" pattern="[A-Za-z]" required>';
-            echo '<input type="submit" value="Submit">';
-            echo '</form>';
+            echo "<form method='post' action=''>";
+            echo "<label for='guess'>Guess a letter:</label>";
+            echo "<input type='text' name='guess' maxlength='1' pattern='[A-Za-z]' required>";
+            echo "<input type='submit' value='Submit'>";
+            echo "</form>";
         }
         ?>
 
